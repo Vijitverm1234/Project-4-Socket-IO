2ccea7b34f2f6de9c4ce6a3ee616d82e4e515a5a
Vijitverm1234 <vijitverma78@gmail.com>
2025-12-12 20:17:41 +0530

    feat(client): making chnges in UI

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const ProfilePage = () => {
   return (
-    <div>
-      <h1>Profile page</h1>
+    <div className='min-h-screen bg-cover bg-no-repeat flex items-center justify-center'>
+      <div>
+        <form action="">
+
+        </form>
+        <img src="" alt="" />
+      </div>
     </div>
   )
 }
