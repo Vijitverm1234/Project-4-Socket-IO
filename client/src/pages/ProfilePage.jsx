49a09c5aedad39488577862740f5f78f3d90dc35
Vijitverm1234 <vijitverma78@gmail.com>
2025-12-13 14:08:48 +0530

    feat(client):UI done

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import assets from '../assests/assets'
+import {useNavigate} from 'react-router-dom'
 const ProfilePage = () => {
+  const [name,setName]=useState("")
+  const [bio,setBio]=useState("")
+  const [selectedImg,setSelectedImg]=useState(null)
+  const navigate=useNavigate()
+  const handleSubmit=async(e)=>{
+      e.preventDefault();
+      navigate('/')
+  }
   return (
     <div className='min-h-screen bg-cover bg-no-repeat flex items-center justify-center'>
-      <div>
-        <form action="">
-
+      <div className='w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 flex items-center justify-between max-sm:flex-col-reverse rounded-lg'>
+        <form action="" className='flex flex-col gap-5 p-10 flex-1' onSubmit={handleSubmit}>
+          <h3 className='text-lg '>Profile Details</h3>
+          <label htmlFor="avatar" className='flex items-center gap-3 cursor-pointer'>
+            <input type="file" id='avatar' accept='.png, .jpeg, .jpg' onChange={(e)=>setSelectedImg(e.target.files[0])} hidden />
+            <img src={selectedImg?URL.createObjectURL(selectedImg):assets.avatar_icon} className={`w-12 h-12 ${selectedImg && 'rounded-full'}`} alt="" />
+            Upload the Profile Image
+          </label>
+          <input type="text" required  placeholder='Enter Your Name' className='p-2 border border-gray-900 text-black rounded-md focus:ring-2 placeholder:text-center' onChange={(e)=>setName(e.target.value)} value={name}
+           />
+           <textarea placeholder='Enter Profile Bio' className='p-2 border border-gray-900 rounded-md text-black focus:ring-2 placeholder:text-center' onChange={(e)=>setBio(e.target.value)} value={bio}>
+            
+           </textarea>
+           <button type='submit' className='bg-gradient-to-r from-purple-400 to-violet-600 text-white rounded-full cursor-pointer p-2'>Save</button>
         </form>
-        <img src="" alt="" />
+        <img src={assets.logo_icon} className='max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10' alt="" />
       </div>
     </div>
   )
